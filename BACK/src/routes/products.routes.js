0bde8beb879907e1ff0dc471cff00b4f669dd7e4
florvillalba21 <florvllb33@gmail.com>
2023-03-21 12:21:53 -0300

    se pisaron los codigos ;p

--- BACK/src/routes/products.routes.js
+++ BACK/src/routes/products.routes.js
@@ -9,16 +9,6 @@ const {
 } = require("../controllers/products.controller");
 
 
-router.get("/Products", getProducts);
-
-router.get("/Products:productId", getProductById);
-
-router.post("/Products", createProduct);
-
-router.put("/Products:productId", updateProductById);
-
-router.delete("/Products:productId", deleteProductById);
-
 //Rutas de los productos
 router.get("/Products", getProducts);
 
