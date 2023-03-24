907fe45ecb4e6ad6ad19291093afc85c7e224521
florvillalba21 <florvllb33@gmail.com>
2023-03-24 17:25:04 -0300

    Mejorando algunas cosas de diseño. Sse creo el form para cargar productos 24/3

--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Login = () => {
   return (
-    <body id="body">
+    <>
       <div className="container-fluid shadow bg-body rounded login">
         <div id="headLogin">
           <h2 id="h2Login">Bienvenida al sistema</h2>
@@ -14,13 +14,14 @@ export const Login = () => {
             placeholder="Ingresa tu nombre de usuario..."
             className="form-control"
           />
-        </div>
-        <div id="endLogin">
+          <div id="endLogin">
           <button id="btnLogin" className="btn btn-primary" type="submit">
             Ingresar
           </button>
         </div>
+        </div>
+        
       </div>
-    </body>
+    </>
   );
 };
