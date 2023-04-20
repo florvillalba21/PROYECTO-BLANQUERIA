509d7b35074086f6b8d3f962541e1e2bc02a42f5
florvillalba21 <florvllb33@gmail.com>
2023-04-20 16:03:43 -0300

    haciendo alerts

---
+++ frontend/src/components/Alert.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function Alert(props) {
+  const [showAlert, setShowAlert] = useState(false);
+
+  useEffect(() => {
+    setShowAlert(props.showAlert);
+  }, [props.showAlert]);
+
+  const handleClick = () => {
+    setShowAlert(false);
+  };
+
+  return (
+    showAlert && (
+      <div className={`${props.type}`}>
+        <span className="close-btn" onClick={handleClick}>
+          ×
+        </span>
+        {props.message}
+      </div>
+    )
+  );
+}
+
+export default Alert;
--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -1,22 +1,22 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Alert from "../components/Alert";
 
 export const Login = () => {
   const navigate = useNavigate();
   const inpUsername = useRef();
+  const [showAlert, setShowAlert] = useState(false);
 
   //Funcion para loguear
   const loguear = async (e) => {
     e.preventDefault;
-
 
     //instanciamos los datos que utilizaremos en la peticion con axios
     const url = "http://localhost:3000/signin";
     const data = {
       username: inpUsername.current.value,
     };
-    
 
     //se intenta la peticion, y si sale todo ok y devuelve el token del usuario, nos redirigimos a la pag principal, y sino, ocurre un error
     try {
@@ -25,20 +25,23 @@ export const Login = () => {
 
       if (token) {
         localStorage.setItem("token", token);
-        if(localStorage.getItem("token")){
+
+        if (localStorage.getItem("token")) {
           navigate("/home");
         }
-        
       }
-
     } catch (error) {
+      setShowAlert(true);
       console.log("mensaje de error", error);
     }
   };
 
   return (
     <>
-      <div className="container-fluid shadow bg-body rounded login">
+      <div
+        className="container-fluid shadow bg-body rounded login"
+        style={{ padding: "10px" }}
+      >
         <div id="headLogin">
           <h2 id="h2Login">Bienvenida al sistema</h2>
         </div>
@@ -48,7 +51,7 @@ export const Login = () => {
             id="inpLogin"
             type="text"
             placeholder="Ingresa tu nombre de usuario..."
-            className="form-control"
+            className="form-control col"
           />
           <div id="endLogin">
             <button
@@ -59,6 +62,14 @@ export const Login = () => {
             >
               Ingresar
             </button>
+          </div>
+          <div>
+            <Alert
+              showAlert={showAlert}
+              type="alert alert-danger"
+              message="Ha ocurrido un error. Compruebe su usuario."
+              onClose={() => setShowAlert(false)}
+            />
           </div>
         </div>
       </div>
