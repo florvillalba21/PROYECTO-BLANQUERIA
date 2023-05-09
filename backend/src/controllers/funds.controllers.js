80975e6fd1f6f057a86e8027559b49964c2e637f
BenitezWalter <fachareloca@gmail.com>
2023-05-09 09:56:20 -0300

    Controlador de filtrar fondos por fecha

--- backend/src/controllers/funds.controllers.js
+++ backend/src/controllers/funds.controllers.js
@@ -9,8 +9,8 @@ ctrlFunds.newFund = async (req, res) => {
   // const username = await Fund.findOne({user}).populate("user")
 
   // console.log(username)
-  console.log(amount)
-  if (amount!= null && date) {
+  console.log(amount);
+  if (amount != null && date) {
     const newFund = new Fund({
       user,
       amount,
@@ -33,19 +33,52 @@ ctrlFunds.newFund = async (req, res) => {
   }
 };
 
+ctrlFunds.getFundsForDate = async (req, res) => {
+  const { month, year } = req.body;
+
+  const regex = new RegExp(`\\b${month}\\b.*\\b${year}\\b`, "i");
+
+  // Hacer la consulta con Mongoose
+
+  // Hacer la consulta con Mongoose
+  Fund.find({ date: { $regex: regex } })
+
+    .then((resultados) => {
+      if (resultados.length > 0) {
+        const totalAmount = resultados.reduce(
+          (total, fund) => total + fund.amount,
+          0
+        );
+
+        res.json({
+          ok: true,
+          filterFunds: resultados,
+          totalAmount: totalAmount,
+        });
+      } else {
+        res.json({
+          ok: false,
+          msg: "No hay archivos con esa fecha",
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
 ctrlFunds.getFundsForUserId = async (req, res) => {
   const user = req.user._id; //toma el id del usuario ingresado
   //busca los fondos del user
   const filterFunds = await Fund.find({ user });
 
   Fund.aggregate([
-    // filtra los documentos de fondo que tienen el id de usuario que se ingresó. 
+    // filtra los documentos de fondo que tienen el id de usuario que se ingresó.
     {
       //matchea el id
-      $match: { user: user }
+      $match: { user: user },
     },
     {
-      
       $group: {
         _id: user,
         fundTotalAmount: { $sum: "$amount" },
@@ -55,9 +88,8 @@ ctrlFunds.getFundsForUserId = async (req, res) => {
     .then((result) => {
       const fundsTotalAmount = result[0].fundTotalAmount;
 
-      
       res.json({
-        totalFunds:filterFunds,
+        totalFunds: filterFunds,
         fundsUserAmount: fundsTotalAmount,
       });
     })
@@ -66,37 +98,41 @@ ctrlFunds.getFundsForUserId = async (req, res) => {
         err,
       });
     });
-
 };
 
 ctrlFunds.getFunds = async (req, res) => {
   try {
     const funds = await Fund.find();
 
-    Fund.aggregate([
-      {
-        
-        $group: {
-          _id: null,
-          fundTotalAmount: { $sum: "$amount" },
+    if (funds.length > 0) {
+      Fund.aggregate([
+        {
+          $group: {
+            _id: null,
+            fundTotalAmount: { $sum: "$amount" },
+          },
         },
-      },
-    ])
-      .then((result) => {
-        const fundsTotalAmount = result[0].fundTotalAmount;
-  
-        
-        res.json({
-          totalFunds:funds,
-          totalAmountFunds: fundsTotalAmount,
+      ])
+        .then((result) => {
+          const fundsTotalAmount = result[0].fundTotalAmount;
+
+          res.json({
+            totalFunds: funds,
+            totalAmountFunds: fundsTotalAmount,
+          });
+        })
+        .catch((err) => {
+          res.json({
+            err,
+          });
         });
+    }
+    else{
+      res.json({
+        ok:false,
+        msg:"No hay fondos registrados"
       })
-      .catch((err) => {
-        res.json({
-          err,
-        });
-      });
-  
+    }
   } catch (error) {
     console.log(error);
   }
--- backend/src/routes/funds.routes.js
+++ backend/src/routes/funds.routes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 
-const {newFund, getFunds, getFundsForUserId} = require("../controllers/funds.controllers")
+const {newFund, getFunds, getFundsForUserId, getFundsForDate} = require("../controllers/funds.controllers")
 const verifyToken = require('../middlewares/authjwt')
 
 router.post("/sendFund",verifyToken, newFund)
 router.get("/getTotalFund",verifyToken, getFunds)
 router.get("/getFundUser",verifyToken, getFundsForUserId)
+router.get("/getFundDate",verifyToken, getFundsForDate)
 
 module.exports = router
